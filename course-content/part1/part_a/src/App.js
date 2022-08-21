c2fceff9bd8ef55a2975102f4f1e598b90d4f8aa
justin <xief008@outlook.com>
2022-08-21 23:08:10 +0800

    Destructuring part1.a example

--- course-content/part1/part_a/src/App.js
+++ course-content/part1/part_a/src/App.js
@@ -1,20 +1,25 @@
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const props = {
+    name: 'Peter',
+    age: 10
+  }
 
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Hello name={props.name} age={props.age} />
     </div>
   )
 }
