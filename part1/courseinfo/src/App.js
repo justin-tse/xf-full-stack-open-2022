301ff9f2fd1916926f67fff6c883484b2ea89598
justin <xief008@outlook.com>
2022-08-21 15:49:11 +0800

    refactor: step4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -10,43 +10,43 @@ const Part = ({ part }) => {
   )
 }
 
-const Content = ({ part1, part2, part3 }) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={part1} />
-      <Part part={part2} />
-      <Part part={part3} />
+      {
+        parts.map(part => <Part key={part.name} part={part} />)
+      }
     </div>
   )
 }
 
-const Total = ({ total }) => {
-
+const Total = ({ parts }) => {
   return (
-    <p>Number of exercises {total}</p>
+    <p>Number of exercises {parts.reduce((preVal, curVal) => preVal + curVal.exercises, 0)}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
