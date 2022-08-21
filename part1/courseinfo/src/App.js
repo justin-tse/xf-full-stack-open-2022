7a89c0fc13d23e807c603cd31a37109383f17208
justin <xief008@outlook.com>
2022-08-21 14:42:33 +0800

    refactor: step2 Content

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,19 +2,21 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>
 }
 
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  )
+}
+
 const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => {
   return (
-    <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
   )
 }
 
