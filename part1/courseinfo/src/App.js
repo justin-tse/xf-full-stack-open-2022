164342d128024ae359287acad316f49ef955703d
justin <xief008@outlook.com>
2022-08-21 15:52:20 +0800

    refactor: step5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,5 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
+const Header = ({ name }) => {
+  return <h1>{name}</h1>
 }
 
 const Part = ({ part }) => {
@@ -21,32 +21,36 @@ const Content = ({ parts }) => {
 }
 
 const Total = ({ parts }) => {
+  console.log(parts)
   return (
     <p>Number of exercises {parts.reduce((preVal, curVal) => preVal + curVal.exercises, 0)}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
