0eb0851d20e714c6fce98c1ab9d4eb8debf1fa85
justin <xief008@outlook.com>
2022-08-22 20:46:13 +0800

    exercises: 1.6: unicafe step5 refactor

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,34 +1,53 @@
 import { useState } from 'react'
 
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+const Button = ({ handleClick }) => {
+  const { handleGood, handleNeutral, handleBad } = handleClick
+  return (
+    <div>
+      <button onClick={handleGood}>{'good'}</button>
+      <button onClick={handleNeutral}>{'neutral'}</button>
+      <button onClick={handleBad}>{'bad'}</button>
+    </div>
+  )
+}
 
-const Statistic = ({ text, counter }) => <div>{text} {counter}</div>
+const Statistic = ({ info }) => {
+  const { good, neutral, bad, total, average, positive } = info
+  return (
+    <div>
+      <StatisticLine text='good' value={good} />
+      <StatisticLine text='neutral' value={neutral} />
+      <StatisticLine text='bad' value={bad} />
+      <StatisticLine text='all' value={total} />
+      <StatisticLine text='average' value={average} />
+      <StatisticLine text='positive' value={positive + '%'} />
+    </div>
+  )
+}
+
+const StatisticLine = ({ text, value }) => <div>{text} {value}</div>
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const total = good + neutral + bad
-  const average = (good - bad) / total || 0
-  const positive = (good / total) * 100 || 0
+  const average = (good - bad) / total
+  const positive = (good / total) * 100
+
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
 
   return (
     <div>
       <h2>give feedback</h2>
-      <Button handleClick={() => setGood(good + 1)} text='good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Button handleClick={{ handleGood, handleNeutral, handleBad }} />
       <h2>statistics</h2>
-      {(!good && !neutral && !bad) ?
-        <div>No feedback given</div> :
-        <div>
-          <Statistic text='good' counter={good} />
-          <Statistic text='neutral' counter={neutral} />
-          <Statistic text='bad' counter={bad} />
-          <Statistic text='all' counter={total} />
-          <Statistic text='average' counter={average} />
-          <Statistic text='positive' counter={positive + '%'} />
-        </div>
+      {
+        !good && !neutral && !bad ?
+          <div>No feedback given</div> :
+          <Statistic info={{ good, neutral, bad, total, average, positive }} />
       }
     </div>
   );
