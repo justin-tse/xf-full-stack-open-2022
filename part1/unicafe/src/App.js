cdfa99ac931bf82db886692857b725ca34c57b99
justin <xief008@outlook.com>
2022-08-22 20:13:19 +0800

    exercises: 1.6: unicafe step2,3,4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,12 +2,15 @@ import { useState } from 'react'
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
-const Counter = ({ text, counter }) => <div>{text} {counter}</div>
+const Statistic = ({ text, counter }) => <div>{text} {counter}</div>
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const average = (good - bad) / total || 0
+  const positive = (good / total) * 100 || 0
 
   return (
     <div>
@@ -16,9 +19,17 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <h2>statistics</h2>
-      <Counter text='good' counter={good} />
-      <Counter text='neutral' counter={neutral} />
-      <Counter text='bad' counter={bad} />
+      {(!good && !neutral && !bad) ?
+        <div>No feedback given</div> :
+        <div>
+          <Statistic text='good' counter={good} />
+          <Statistic text='neutral' counter={neutral} />
+          <Statistic text='bad' counter={bad} />
+          <Statistic text='all' counter={total} />
+          <Statistic text='average' counter={average} />
+          <Statistic text='positive' counter={positive + '%'} />
+        </div>
+      }
     </div>
   );
 }
