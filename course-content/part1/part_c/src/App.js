74e55bb6eb653f5ceaf857eda1969ed6c4367655
justin <xief008@outlook.com>
2022-08-22 18:11:33 +0800

    part1.d example Function that returns a function

--- course-content/part1/part_c/src/App.js
+++ course-content/part1/part_c/src/App.js
@@ -3,15 +3,24 @@ import { useState } from 'react'
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const handleClick = () => {
-    console.log('clicked the button')
-    setValue(0)
+  const hello = who => () => console.log('hello', who)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
       {value}
-      <button onClick={handleClick}>button</button>
+      <br />
+      <button onClick={() => setToValue(1000)}>thousand</button>
+      <button onClick={() => setToValue(0)}>reset</button>
+      <button onClick={() => setToValue(value + 1)}>increment</button>
+      <br />
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
     </div>
   )
 }
