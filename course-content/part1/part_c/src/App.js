9125d2e58f19dd719593f4f00531923ef083bb85
justin <xief008@outlook.com>
2022-08-22 12:36:01 +0800

    part1.d example conditional rendering

--- course-content/part1/part_c/src/App.js
+++ course-content/part1/part_c/src/App.js
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 
+const History = ({ allClicks }) => {
+  console.log(allClicks)
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {allClicks.join('-')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
@@ -18,14 +36,10 @@ const App = () => {
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
+      <Button handleClick={handleLeftClick} text={'left'} />
+      <Button handleClick={handleRightClick} text={'right'} />
       {right}
-      <p>{allClicks.join('-')}</p>
+      <History allClicks={allClicks} />
     </div>
   )
 }
