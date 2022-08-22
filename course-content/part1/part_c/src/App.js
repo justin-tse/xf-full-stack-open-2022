c7cc1dc92200704787ff81d37d60c110696f7764
justin <xief008@outlook.com>
2022-08-22 14:26:35 +0800

    part1.d example Event Handling Revisited

--- course-content/part1/part_c/src/App.js
+++ course-content/part1/part_c/src/App.js
@@ -1,47 +1,19 @@
 import { useState } from 'react'
 
-const History = ({ allClicks }) => {
-  console.log(allClicks)
-  if (allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {allClicks.join('-')}
-    </div>
-  )
-}
-
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
-
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const handleClick = () => {
+    console.log('clicked the button')
+    setValue(0)
   }
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text={'left'} />
-      <Button handleClick={handleRightClick} text={'right'} />
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={handleClick}>button</button>
     </div>
   )
 }
 
-export default App;
+export default App
