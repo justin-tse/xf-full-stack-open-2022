af12e4bc7517d19f9ec32ff3986c5100415e41c9
justin <xief008@outlook.com>
2022-08-23 10:38:02 +0800

    feat: anecdotes step2 add votes function

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,17 +10,43 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
-
   const [select, setSelect] = useState(anecdotes[0])
+
+  // 1. initialCounters use array
+  // const initialCounters = new Array(anecdotes.length).fill(0)
+  
+  // 2. initialCounters use object
+  const initialCounters = new Map()
+  for (let i = 0; i <= anecdotes.length; i++) {
+    initialCounters[i] = 0
+  }
+
+  const [counters, setCounters] = useState(initialCounters)
+
+  const selectedIndex = anecdotes.indexOf(select)
+
   const handleNextAnecdote = () => {
     const randomIndex = (Math.random() * (anecdotes.length - 1)).toFixed(0)
     console.log(randomIndex)
     setSelect(anecdotes[randomIndex])
+    return randomIndex
+  }
+
+  const handleVote = () => {
+    // 1. initialCounters use array
+    // const copy = [...counters]
+    // copy[selectedIndex]++
+    // setCounters(copy)
+
+    // 2. initialCounters use object
+    setCounters({ ...counters, [selectedIndex]: counters[selectedIndex] + 1 })
   }
 
   return (
     <div>
       <p>{select}</p>
+      <p>has {counters[selectedIndex]} votes</p>
+      <button onClick={handleVote}>vote</button>
       <button onClick={handleNextAnecdote}>next anecdote</button>
     </div>
   );
