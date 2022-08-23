349194e77b6f7b539cb48ed9fb3e92ba9cbe8976
justin <xief008@outlook.com>
2022-08-23 20:33:56 +0800

    feat: courseinfo step9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -23,7 +23,7 @@ const Content = ({ parts }) => {
   )
 }
 
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ course }) => <h3>{course}</h3>
 
 const Course = ({ course }) => {
   const { name, parts } = course
@@ -37,34 +37,61 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      <h2>Web development curriculum</h2>
+      {
+        courses.map(course =>
+          <Course key={course.id} course={course} />
+        )
+      }
+    </div>
+  )
 }
 
 export default App
