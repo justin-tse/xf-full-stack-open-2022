4428bd8f3d71af4c00b2dba514c2dde08e2fdd1f
justin <xief008@outlook.com>
2022-08-24 11:42:49 +0800

    chore: course-content part2.b forms filtering displayed elements

--- course-content/part2/note/src/App.js
+++ course-content/part2/note/src/App.js
@@ -4,6 +4,7 @@ import Note from "./components/Note";
 const App = props => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = event => {
     event.preventDefault()
@@ -23,11 +24,20 @@ const App = props => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note =>
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
