b967f92473f6c628c3d96dcbc31f404be04a47b0
justin <xief008@outlook.com>
2022-08-24 14:49:24 +0800

    feat: the phonebook step1 controlling the form input element

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,21 +2,34 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' },
+    { name: 'Justin' },
   ])
   const [newName, setNewName] = useState('')
+
+  const handlePersonsChange = event => setNewName(event.target.value)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    setPersons(persons.concat({ name: newName }))
+    setNewName('')
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form >
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handlePersonsChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleSubmit}>add</button>
         </div>
       </form>
+      <h2>Numbers</h2>
+      {
+        persons.map(person => <div key={person.name}>{person.name}</div>)
+      }
     </div>
   )
 }
