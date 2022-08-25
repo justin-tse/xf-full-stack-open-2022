d14d8de6071baa086b52940025d5317fb645d21d
justin <xief008@outlook.com>
2022-08-25 10:08:46 +0800

    feat: course-content part2.c getting data from server axios and promise

--- course-content/part2/note/src/index.js
+++ course-content/part2/note/src/index.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import axios from 'axios';
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App notes={notes} />);
+const promise = axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    console.log(response)
+    const notes = response.data
+    console.log(notes)
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<App notes={notes} />);
+  })
