c55219548ba3bb2ddd1259c80340083ac06fcf4f
justin <xief008@outlook.com>
2022-08-25 11:27:09 +0800

    refactor: the phonebook step6 getting data from json-server by using axios and useEffect

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Input = ({ text, value, onChange }) => {
   return (
@@ -40,15 +41,22 @@ const Persons = ({ filterPersons }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [filterPersons, setFilterPersons] = useState(persons)
+
+  useEffect((() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => {
+        console.log('promise fulfilled', res)
+        setPersons(res.data)
+        setFilterPersons(res.data)
+      })
+  }), [])
+
+  const [filterPersons, setFilterPersons] = useState([])
 
   const handleNameChange = event => setNewName(event.target.value)
 
