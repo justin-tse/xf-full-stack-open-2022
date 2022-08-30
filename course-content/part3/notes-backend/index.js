91354f29676f95c080b4e982a1cf2cf54f4cb355
justin <xief008@outlook.com>
2022-08-30 21:24:31 +0800

    refactor: part3 notes-backend use express

--- course-content/part3/notes-backend/index.js
+++ course-content/part3/notes-backend/index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 const notes = [
   {
@@ -21,9 +22,12 @@ const notes = [
   }
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+app.get('/', (resquest, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 
 const PORT = 3001
