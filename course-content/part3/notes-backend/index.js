01f0cd2506c2fad75f7f324a7b1469757174a575
justin <xief008@outlook.com>
2022-08-30 18:14:12 +0800

    feat: part3 notes-backend simple web server

--- course-content/part3/notes-backend/index.js
+++ course-content/part3/notes-backend/index.js
@@ -1,1 +1,31 @@
-console.log("hello world")
+const http = require('http')
+
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(notes))
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
