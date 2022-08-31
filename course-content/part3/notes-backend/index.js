9b4fdd6797a5411de10539c0236a3e5d2089e69f
justin <xief008@outlook.com>
2022-08-31 20:45:13 +0800

    feat: part3 notes-backend - fetching a single resource

--- course-content/part3/notes-backend/index.js
+++ course-content/part3/notes-backend/index.js
@@ -30,6 +30,17 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.statusMessage = "haha the page can not found, 404"
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
