3796710a9a96bbc8887c5958f28d5f1197cc4401
justin <xief008@outlook.com>
2022-09-06 15:43:21 +0800

    feat: part3 phonebook-backend step2,3 info and find person

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,4 +1,6 @@
-const http = require('http')
+const { response } = require("express")
+const express = require('express')
+const app = express()
 
 const persons = [
   {
@@ -23,11 +25,28 @@ const persons = [
   }
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end(JSON.stringify(persons))
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World</h1>')
 })
 
+app.get('/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`<div>Phonebook has info for ${persons.length} people</div> 
+  <p>${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(400).end("Can not find person")
+  }
+})
 const PORT = 3001
 app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+console.log(`Server on ${PORT}`)
