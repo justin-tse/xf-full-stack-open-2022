90caa503759060d41898a259fdeeaed0ed438de0
justin <xief008@outlook.com>
2022-09-08 18:27:29 +0800

    chore: part3 phonebook-backend step10,11 deploy the full stack to the internet

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -9,7 +9,7 @@ const getAll = () => {
 
 const create = newObject => {
   const request = axios.post(baseUrl, newObject)
- 
+
   return request.then(res => res.data)
 }
 
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -26,11 +26,14 @@ let persons = [
   }
 ]
 
-app.use(cors())
 
 app.use(express.json())
 
 app.use(morgan('tiny'))
+
+app.use(cors())
+
+app.use(express.static('build'))
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':data'))
@@ -100,6 +103,6 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server on ${PORT}`)
