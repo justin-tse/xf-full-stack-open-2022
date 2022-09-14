d631b147d05008b8b0372da6b7a882e19158da04
justin <xief008@outlook.com>
2022-09-14 16:50:45 +0800

    refactor: part3 notes-backend Error handling

--- course-content/part3/notes-backend/index.js
+++ course-content/part3/notes-backend/index.js
@@ -32,9 +32,18 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.post('/api/notes', (request, response) => {
