28380aa06b13a1b62a15eae213a4ab5b8b877ce2
justin <xief008@outlook.com>
2022-09-14 10:52:59 +0800

    chore: part3 step12 phonebook-backend use MongoDB Atlas as db and store and fetch data

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+if (process.argv.length < 3) {
+  console.log('Please input the password as the third argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://xf-fullstack-2022:${password}@cluster0.onlalo2.mongodb.net/phonkbookApp?retryWrites=true&w=majority`
+
+const personSchema = new Schema({
+  name: String,
+  number: Number,
+})
+
+const Person = mongoose.model('person', personSchema)
+
+mongoose
+  .connect(url)
+  .then(result => {
+    console.log('connect')
+    if (process.argv.length > 3) {
+      const name = process.argv[3]
+      const number = process.argv[4]
+      const person = new Person({
+        name: name,
+        number: number
+      })
+      person.save().then(res => {
+        mongoose.connection.close()
+      })
+    } else {
+      console.log('person:')
+      Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person.name, person.number)
+        })
+      }).then(res => {
+        mongoose.connection.close()
+      })
+    }
+  })
