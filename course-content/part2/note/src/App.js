5162c0f20c32eec9db6d96bd601206d500c96c3f
justin <xief008@outlook.com>
2022-09-14 16:36:01 +0800

    fix: part2 notes error message at the begining

--- course-content/part2/note/src/App.js
+++ course-content/part2/note/src/App.js
@@ -22,7 +22,7 @@ const App = props => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     console.log('effect')
