a9dff2c002c869f5a43297c3c39688b2805da306
justin <xief008@outlook.com>
2022-09-16 10:56:00 +0800

    fix: part3 notes-backend Validation

--- course-content/part3/notes-backend/index.js
+++ course-content/part3/notes-backend/index.js
@@ -46,12 +46,6 @@ app.get('/api/notes/:id', (request, response, next) => {
 app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
