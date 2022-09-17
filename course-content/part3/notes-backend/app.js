214c1da4ba3b7f62008d96127e55bd5f693bfcd4
justin <xief008@outlook.com>
2022-09-17 14:20:52 +0800

    refactor: part4 notes-backend change project structure

---
+++ course-content/part3/notes-backend/app.js
@@ -0,0 +1,34 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+// the front end static webside
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/notes', notesRouter)
+
+// handler of requests with unknown endpoint
+app.use(middleware.unknownEndpoint)
+
+// handler of requests with result to errors
+app.use(middleware.errorHandler)
+
+module.exports = app
--- course-content/part3/notes-backend/models/note.js
+++ course-content/part3/notes-backend/models/note.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
