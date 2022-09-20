8a25d452d72a7e5db57e6fcba16075dbe1e0ce5e
justin <xief008@outlook.com>
2022-09-20 10:54:54 +0800

    test: part4 bloglist-backend exercises4.3 test node app

---
+++ part4/bloglist-backend/test/dummy.test.js
@@ -0,0 +1,8 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+  if (listHelper === 5) { return }
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
