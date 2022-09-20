bb6ab52e98e606e4b27ddf67da33df3fb7a9636f
justin <xief008@outlook.com>
2022-09-20 16:18:14 +0800

    test: part4 bloglist-backend exercises4.4 helper functions and unit tests, step2

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0
+  if (blogs.length === 1) return blogs[0].likes
+  const likesArr = blogs.map(blog => blog.likes)
+  return likesArr.reduce((prev, curv) => prev + curv)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
