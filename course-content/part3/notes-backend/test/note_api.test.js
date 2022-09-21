d997a9b400bcb17ec58b860650bb0e9fcb46202e
justin <xief008@outlook.com>
2022-09-21 11:31:53 +0800

    test: part4 notes-backend supertest

---
+++ course-content/part3/notes-backend/test/note_api.test.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body).toHaveLength(2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body[0].content).toBe('HTML is easy')
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
