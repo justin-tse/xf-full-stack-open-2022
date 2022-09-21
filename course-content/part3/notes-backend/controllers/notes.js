045d9f81d1673d4dcd30c3f501e3f80eb1be1345
justin <xief008@outlook.com>
2022-09-21 22:18:34 +0800

    refactor: part4 notes-backend Eliminating the try-catch

--- course-content/part3/notes-backend/controllers/notes.js
+++ course-content/part3/notes-backend/controllers/notes.js
@@ -6,20 +6,16 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
-notesRouter.get('/:id', async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id)
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  } catch (error) {
-    next(error)
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
   }
 })
 
-notesRouter.post('/', async (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -28,12 +24,8 @@ notesRouter.post('/', async (request, response, next) => {
     date: new Date(),
   })
 
-  try {
-    const saveNode = await note.save()
-    response.status(201).json(saveNode)
-  } catch (error) {
-    next(error)
-  }
+  const saveNode = await note.save()
+  response.status(201).json(saveNode)
 })
 
 notesRouter.put('/:id', (request, response, next) => {
@@ -48,13 +40,9 @@ notesRouter.put('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-notesRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Note.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  } catch (error) {
-    next(error)
-  }
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = notesRouter
