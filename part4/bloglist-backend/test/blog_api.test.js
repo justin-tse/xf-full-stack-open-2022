5e1c3b07984fc10b94da215645818fccd0d04776
justin <xief008@outlook.com>
2022-09-25 23:07:09 +0800

    test: part4 bloglist-backend exercises4.9 toBeDefined check id

--- part4/bloglist-backend/test/blog_api.test.js
+++ part4/bloglist-backend/test/blog_api.test.js
@@ -9,6 +9,14 @@ test('blogs are returned as json', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('the unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  const ids = response.body.map(res => res.id)
+  console.log(response.body, ids)
+  ids.forEach(id => expect(id).toBeDefined())
 })
 
 afterAll(() => {
