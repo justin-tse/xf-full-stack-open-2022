34f2f1a81431c8fbd66b2e5adc3a4ac10036ddff
justin <xief008@outlook.com>
2022-09-26 22:37:55 +0800

    test: part4 bloglist-backend exercises4.10 HTTP POST

--- part4/bloglist-backend/test/blog_api.test.js
+++ part4/bloglist-backend/test/blog_api.test.js
@@ -3,6 +3,15 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+const Blog = require('../models/blog')
+const blogsList = require('../utils/list_helper').blogs
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blogs = blogsList.map(blog => new Blog(blog))
+  const blogArray = blogs.map(blog => blog.save())
+  await Promise.all(blogArray)
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -17,6 +26,23 @@ test('the unique identifier property of the blog posts is named id', async () =>
   const ids = response.body.map(res => res.id)
   console.log(response.body, ids)
   ids.forEach(id => expect(id).toBeDefined())
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = new Blog({
+    title: 'hello'
+  })
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+
+  const blogsInDb = async () => {
+    const blogs = await Blog.find({})
+    return blogs.map(blog => blog.toJSON())
+  }
+  const blogsAtEnd = await blogsInDb()
+  expect(blogsAtEnd).toHaveLength(blogsList.length + 1)
 })
 
 afterAll(() => {
