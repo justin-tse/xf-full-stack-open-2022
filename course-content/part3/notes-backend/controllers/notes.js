69edfb9da518af593eb2dc363d8cd412ea34f06e
justin <xief008@outlook.com>
2022-09-30 00:20:22 +0800

    feat: part4 notes-backend Creating a new note

--- course-content/part3/notes-backend/controllers/notes.js
+++ course-content/part3/notes-backend/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({})
@@ -18,14 +19,20 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user._id,
   })
 
-  const saveNode = await note.save()
-  response.status(201).json(saveNode)
+  const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  response.json(savedNote)
 })
 
 notesRouter.put('/:id', (request, response, next) => {
